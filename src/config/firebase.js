4cf1d0ce61426b62e6f5050ba842a1ce0cf4db9d
saad-1719 <chsaad1407@gmail.com>
2024-12-16 19:10:40 +0500

    add Firebase configuration and authentication functions

---
+++ src/config/firebase.js
@@ -0,0 +1,76 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getFirestore, setDoc, doc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCEuBou4HnlwewDR7Df_EBE8Kb8V-rQw2k",
+    authDomain: "linkup-c94af.firebaseapp.com",
+    projectId: "linkup-c94af",
+    storageBucket: "linkup-c94af.firebasestorage.app",
+    messagingSenderId: "857642121978",
+    appId: "1:857642121978:web:55a3428e7878a23a36a743"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (username, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        
+        // Create user document in Firestore
+        await setDoc(doc(db, "users", user.uid), {
+            id: user.uid,
+            username: username.toLowerCase(),
+            email,
+            name: "",
+            avatar: "",
+            bio: "Hey, I am using Likup",
+            lastSeen: Date.now(),
+        });
+
+        // Create chats document for the user
+        await setDoc(doc(db, "chats", user.uid), {
+            chatData: []
+        });
+toast.success("Account created successfully!");
+        return user; // Optionally return the user object
+    } catch (error) {
+        console.error(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+        throw error; // Re-throw the error for handling in the calling function
+    }
+}
+
+const login = async (email, password) =>
+{
+    try {
+        
+        await signInWithEmailAndPassword(auth, email, password);
+        toast.success("Login Successfull !!!");
+    } catch (error) {
+        console.error(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+        throw error; 
+    }
+}
+const logout = async () =>
+{
+    try {
+        await signOut(auth);
+        toast.success("Logout Successfully !!!");
+
+        
+    } catch (error) {
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+        throw error; 
+    }
+}
+export { signup,login,logout,auth,db };
