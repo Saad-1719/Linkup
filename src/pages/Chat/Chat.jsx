15913b09da9135dec5bfd68158767d992f2ce0e8
saad-1719 <chsaad1407@gmail.com>
2024-12-17 20:54:54 +0500

    feat: add loading state to Chat component for improved user experience

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import LeftSideBar from "../../components/LeftSideBar";
 import ChatBox from "../../components/ChatBox";
 import RightSideBar from "../../components/RightSideBar";
+import { AppContext } from "../../context/AppContext";
 
-const Chat = () => {
+const Chat = () =>
+{
+  
+  const { chatData, userData } = useContext(AppContext);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    if (chatData && userData)
+    {
+      setLoading(false);
+    }
+},[chatData,userData])
 	return (
-		<div className="min-h-screen bg-blue-800 flex justify-center items-center">
-      <div className="w-[95%] h-[75vh] py-auto max-h-screena bg-blue-200  grid grid-cols-[1fr_2fr_1fr]">
+		<div className="min-h-screen bg-sky-700 flex justify-center items-center">
+      {
+        loading ?
+          
+          <p>Loading ...</p> :
+          
+      <div className="w-[90%] h-[85vh] py-auto max-h-screena bg-blue-200  grid grid-cols-[1fr_2fr_1fr] rounded-lg">
         <LeftSideBar />
         <ChatBox />
         <RightSideBar/>
 
       </div>
+      }
 		</div>
 	);
 };
